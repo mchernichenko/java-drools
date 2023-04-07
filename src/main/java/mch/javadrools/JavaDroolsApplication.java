62387d177679681eacf9574f347ca68c957741be
Mikhail.Chernichenko <mchernichenko@mail.ru>
2023-04-07 14:42:02 +0300

    init

---
+++ src/main/java/mch/javadrools/JavaDroolsApplication.java
@@ -0,0 +1,13 @@
+package mch.javadrools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDroolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mch/javadrools/JavaDroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package mch.javadrools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDroolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
