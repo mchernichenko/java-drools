837d636c7ac56e0313e83de8284155cf04cd2f81
Mikhail.Chernichenko <mchernichenko@mail.ru>
2023-04-11 14:06:19 +0300

    temp

--- src/main/java/mch/javadrools/Application.java
+++ src/main/java/mch/javadrools/Application.java
@@ -1,16 +1,13 @@
 package mch.javadrools;
 
-import org.kie.api.KieServices;
-import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class JavaDroolsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaDroolsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 /*
 	@Bean
--- src/test/java/mch/javadrools/TaxiFareCalculatorServiceIntegrationTest.java
+++ src/test/java/mch/javadrools/TaxiFareCalculatorServiceIntegrationTest.java
@@ -5,20 +5,54 @@ import mch.javadrools.model.Fare;
 import mch.javadrools.model.TaxiRide;
 import mch.javadrools.service.TaxiFareCalculatorService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TaxiFareConfiguration.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+//@Import(TaxiFareConfiguration.class)
+//@SpringApplicationConfiguration(classes = TaxiFareConfiguration.class)
 public class TaxiFareCalculatorServiceIntegrationTest {
+    private static final String URL = "/api/calcTaxiFare";
 
     @Autowired
+    private MockMvc mockMvc;
+    @Autowired
     private TaxiFareCalculatorService taxiFareCalculatorService;
+    @Test
+    void testCreatePerson() throws Exception {
+        MockHttpServletResponse responsePost = mockMvc
+                .perform(post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"isNightSurcharge\": false, \"distanceInMile\": 9}")  // Добавляем содержимое тела
+                )
+                .andReturn()
+                .getResponse();
+
+        assertThat(responsePost.getStatus()).isEqualTo(200);
+   //     assertThat(responsePost.getContentAsString()).contains("70");
+    }
 
     @Test
     public void whenNightSurchargeFalseAndDistanceLessThan10_thenFixFareWithoutNightSurcharge() {
